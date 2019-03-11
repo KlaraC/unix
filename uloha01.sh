dcf888b2aab81e9f0044e585723a5cea58036584
Klara Cihlarova <klaracihlarova98@seznam.cz>
2019-03-11 19:14:32 +0100

    Prvni domaci uloha

--- uloha01.sh
+++ uloha01.sh
@@ -1,24 +1,4 @@
-test -h $2
-if [ $? == 0 ]; then 
-a=0
-else a=1
-fi
-test -d $2
-if [ $? == 0 ]; then
-b=0
-else b=1
-fi
-test -f $2
-if [ $? == 0 ]; then
-c=0
-else c=1
-fi
-test -e $2
-if [ $? == 0 ]; then
-d=0 
-else d=1
-fi
-if [ $1 == "--help" ]; then
+if [ "$1" == "--help" ]; then
 echo "Pouziti: uloha01.sh [--typ|--help] [cesta_k_souboru]
   --typ   Vypise typ souboru popsaneho danou cestou, pokud neexistuje, napise: Neexistuje
   --help  Vypise navod na pouziti tohoto programu
@@ -26,14 +6,23 @@ Exit status
   0   soubor existuje
   1   soubor neexistuje"
 elif [  "$1" == "--typ" ]; then
- if [ $a == 0 ]; then 
+ if [ -h "$2" ]; then 
    echo "Symbolicky link"
- elif [ $b == 0 ]; then
+ elif [ -d "$2" ]; then
    echo "Adresar"
- elif [ $c == 0 ]; then 
+ elif [ -f "$2" ]; then 
    echo "Soubor"
- elif [ $d == 1 ]; then 
+ elif [ -c "$2" ]; then
+ echo "znakove zarizeni"
+ elif [ -b "$2" ]; then
+ echo "blokove zarizeni"
+ elif [ -p "$2" ]; then
+ echo "fifo"
+ elif [ -e "$2" ]; then 
+  exit 0
+  else
   echo "Neexistuje"
   exit 1
 fi
+else echo "Chyba"
 fi
