b3b6fb66a81733a0377e9cf707a5b5191fed767e
Klara Cihlarova <klaracihlarova98@seznam.cz>
2019-04-08 22:55:32 +0200

    Prvni cast tretiho ukolu

---
+++ middle.sh
@@ -0,0 +1,39 @@
+if [ "$1" == "-l" ] || [ "$1" == "--lines" ]; then
+head -$(echo "$2"|cut -d - -f 2) | tail +$(echo "$2" | cut -d - -f 1);
+elif [ "$1" == "-f" ] || [ "$1" == "--fraction" ]; then
+cat > /tmp/N;
+a=`echo "$3" |cut -d / -f 1`;
+b=`echo "$3" |cut -d / -f 2`;
+c=`cat /tmp/N | wc -l`;
+a=$(( $a * $c ));
+ex=`expr $a % $b` ;
+if [ $ex == 0 ]; then
+a=$(( $a / $b ));
+else
+a=$(( $a / $b ));
+a=$(( $a + 1 ));
+fi
+b=`echo "$2" |cut -d / -f 1`;
+d=`echo "$2" |cut -d / -f 2`;
+b=$(( $b * $c ));
+b=$(( $b / $d ));
+cat /tmp/N | head -$a | tail +$b ;
+elif [ "$1" == "-p" ] || [ "$1" == "--part" ]; then
+cat > /tmp/N;
+c=`cat /tmp/N | wc -l`;
+a=`echo "$2" |cut -d / -f 1`;
+b=`echo "$2" |cut -d / -f 2`;
+e=$(( $c % $b ));
+f=$b;
+b=$(( $c / $b ));
+if [ $e -gt 0 ] && [ $a == $f ]; then
+f=$b;
+b=$(( $b + $e ));
+else
+f=$b;
+fi
+a=$(( $a - 1 ));
+a=$(( $a * $f ));
+a=$(( $a + 1 ));
+cat /tmp/N | tail +$a | head -$b ; 
+fi
